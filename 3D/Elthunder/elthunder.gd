f9211ecb7ea85d85381f7763ed49d2ca1ce0794b
Keelan <hui.keelan@gmail.com>
2026-06-02 00:50:02 -0700

    Add larger hitbox for elthunder

--- 3D/Elthunder/elthunder.gd
+++ 3D/Elthunder/elthunder.gd
@@ -1,1 +1,13 @@
 extends Node3D
+
+@export var screen_flash_scene: PackedScene
+
+func _ready() -> void:
+	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
+	
+func _on_animation_finished(anim_name: String):
+	queue_free()
+
+func call_screen_flash():
+	add_child(screen_flash_scene.instantiate())
+	
